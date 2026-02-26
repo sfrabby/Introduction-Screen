2df42faf114bd87254c662a78b32600f79a96b5c
sfrabby <fazlerabby6732@gmail.com>
2026-02-27 01:19:05 +0600

    intl package added and time picker added

--- lib/view/Home Page/ui.dart
+++ lib/view/Home Page/ui.dart
@@ -3,6 +3,7 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:intorduction_screen/main.dart';
+import 'package:timezone/timezone.dart' as tz;
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -19,11 +20,15 @@ class HomePage extends StatelessWidget {
     NotificationDetails notificationDetails = NotificationDetails(
       android: androidNotificationDetails,
     );
-     await notificationsPlugin.show(
+    DateTime? scheduldate = DateTime.now().add(Duration(seconds: 5));
+    await notificationsPlugin.zonedSchedule(
+      tz.TZDateTime.from(scheduldate, tz.local),
       id: 0,
       body: "This is Local Notification",
       notificationDetails: notificationDetails,
       title: "Simple",
+      androidScheduleMode: AndroidScheduleMode.exactAllowWhileIdle, scheduledDate: scheduldate,
+
     );
   }
 
@@ -35,17 +40,32 @@ class HomePage extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          ElevatedButton(onPressed: showNotification , child: Text("Local Notification ")),
-      ElevatedButton(onPressed: () async {
-        DateTime?  date = await showDatePicker(context: context, firstDate: DateTime.now().subtract(Duration(days: 365)), lastDate: DateTime.now());
-        print(date);
-      } , child: Text("Date picker ")),
+          ElevatedButton(
+            onPressed: showNotification,
+            child: Text("Local Notification "),
+          ),
+          ElevatedButton(
+            onPressed: () async {
+              DateTime? date = await showDatePicker(
+                context: context,
+                firstDate: DateTime.now().subtract(Duration(days: 365)),
+                lastDate: DateTime.now(),
+              );
+              print(date);
+            },
+            child: Text("Date picker "),
+          ),
 
-        ElevatedButton(onPressed: showNotification , child: Text("Local Notification ")),
+          ElevatedButton(
+            onPressed: showNotification,
+            child: Text("Local Notification "),
+          ),
 
-    ElevatedButton(onPressed: showNotification , child: Text("Local Notification ")),
-
-    ],
+          ElevatedButton(
+            onPressed: showNotification,
+            child: Text("Local Notification "),
+          ),
+        ],
       ),
     );
   }
