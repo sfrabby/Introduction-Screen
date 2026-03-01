abe1e1426f5fb82b8a84c7d83e1b6d9f293abe5d
sfrabby <fazlerabby6732@gmail.com>
2026-03-01 23:11:19 +0600

    Hive database start

---
+++ lib/view/Hive CRUD/ui.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_connect/http/src/utils/utils.dart';
+
+class HiveNoteApp extends StatelessWidget {
+  const HiveNoteApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(centerTitle: true,
+      backgroundColor: Colors.teal,
+      title: Text("Hive Database"),),
+    );
+  }
+}
