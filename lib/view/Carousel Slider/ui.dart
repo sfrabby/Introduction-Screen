b088768e29395c5e06ca6bd6b2db4bedfc540beb
sfrabby <fazlerabby6732@gmail.com>
2026-03-07 23:29:51 +0600

    UI update

--- lib/view/Carousel Slider/ui.dart
+++ lib/view/Carousel Slider/ui.dart
@@ -8,6 +8,33 @@ class carouselSlider extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(backgroundColor: Colors.teal,
       title: Text("Carousel Slider", style: TextStyle(color: Colors.white),),),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+
+        children: [
+          SizedBox(
+            height: 200,
+            width: double.infinity,
+            child: CarouselSlider(
+              options: CarouselOptions(height: 400.0),
+              items: [1,2,3,4,5].map((i) {
+                return Builder(
+                  builder: (BuildContext context) {
+                    return Container(
+                        width: MediaQuery.of(context).size.width,
+                        margin: EdgeInsets.symmetric(horizontal: 5.0),
+                        decoration: BoxDecoration(
+                            color: Colors.amber
+                        ),
+                        child: Text('text $i', style: TextStyle(fontSize: 16.0),)
+                    );
+                  },
+                );
+              }).toList(),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
