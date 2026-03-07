4edc5590f39efea8c4f9f5742eca5ec0969eeae4
sfrabby <fazlerabby6732@gmail.com>
2026-03-07 23:39:27 +0600

    Done

--- lib/view/Carousel Slider/ui.dart
+++ lib/view/Carousel Slider/ui.dart
@@ -1,8 +1,15 @@
+import 'package:carousel_slider/carousel_options.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
-class carouselSlider extends StatelessWidget {
+class carouselSlider extends StatefulWidget {
   const carouselSlider({super.key});
 
+  @override
+  State<carouselSlider> createState() => _carouselSliderState();
+}
+
+class _carouselSliderState extends State<carouselSlider> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,7 +22,7 @@ class carouselSlider extends StatelessWidget {
           SizedBox(
             height: 200,
             width: double.infinity,
-            child: CarouselSlider(
+            child:  CarouselSlider(
               options: CarouselOptions(height: 400.0),
               items: [1,2,3,4,5].map((i) {
                 return Builder(
